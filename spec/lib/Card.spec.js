69e8c9ac4018da4e7c093047bd3d7d80a857761c
Jonathan Davidson <jdavidson@netsplasher.com>
2017-11-25 16:11:47 -0600

    Added displayName to Card class

--- spec/lib/Card.spec.js
+++ spec/lib/Card.spec.js
@@ -7,6 +7,72 @@ const jack = Card.values[10];
 const queen = Card.values[11];
 const king = Card.values[12];
 
+describe('Card', () => {
+  describe('Initializes properly', () => {
+    const name = 'name';
+    const values = [1, 2, 3];
+    const card = new Card.Card(name, 'clubs', values);
+
+    it('Card is instantiated with a name', () => {
+      expect(card.name).toEqual(name);
+    });
+
+    it('Card is instantiated with an array of values', () => {
+      expect(card.values).toEqual(values);
+    });
+  });
+
+  describe('getLowValue()', () => {
+    it('returns the lowest value', () => {
+      const card = new Card.Card('ace', 'clubs', [1, 11]);
+      expect(card.getLowValue()).toBe(1);
+    });
+  });
+
+  describe('isAce()', () => {
+    it('identifies ace', () => {
+      const card = new Card.Card('ace', 'clubs', [1, 11]);
+      expect(card.isAce()).toBe(true);
+    });
+
+    it('identifies non-ace', () => {
+      const card = new Card.Card('two', 'clubs', [2]);
+      expect(card.isAce()).toBe(false);
+    });
+  });
+
+  describe('isShuffleMarker', () => {
+    it('returns true when the card is a shuffle marker', () => {
+      const card = new Card.Card('shuffleMarker', null, null, true);
+      expect(card.isShuffleMarker()).toEqual(true);
+    });
+
+    it('returns false when not a shuffle marker', () => {
+      const card = new Card.Card('ace', 'clubs', [1, 11]);
+      expect(card.isShuffleMarker()).toEqual(false);
+    });
+  });
+
+  describe('isTen', () => {
+    it('returns true when the value is 10', () => {
+      const card = new Card.Card('ten', 'clubs', [10]);
+      expect(card.isTen()).toBe(true);
+    });
+
+    it('returns false when the value is not 10', () => {
+      const card = new Card.Card('two', 'clubs', [2]);
+      expect(card.isTen()).toBe(false);
+    });
+  });
+
+  describe('getDisplayName()', () => {
+    it('returns the displayName', () => {
+      const card = new Card.Card('ace', 'spades', [1, 11]);
+      expect(card.getDisplayName()).toEqual('ace of spades');
+    });
+  });
+});
+
 describe('lib/Card', () => {
   describe('generateShuffleMarker', () => {
     const shuffleMarker = Card.generateShuffleMarker();
@@ -100,7 +166,7 @@ describe('lib/Card', () => {
     });
 
     it('identifies non-ten', () => {
-      expect(Card.isAce(two)).toBe(false);
+      expect(Card.isTen(two)).toBe(false);
     });
   });
 
